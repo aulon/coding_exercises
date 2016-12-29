c874b74d9e585d16be6c4f3e683ca215e30a1c9f
aulon <aulonkuqi@gmail.com>
2016-12-29 22:38:32 +0100

    Fixing matrix problem

--- Arrays_and_Strings/c++/matrix_rotation.cpp
+++ Arrays_and_Strings/c++/matrix_rotation.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-int matrix_rotation(int n){
+void matrix_rotation(int n){
 	int i,j;
 	int matrix[n][n];
 	int count = 1;
@@ -39,9 +39,52 @@ int matrix_rotation(int n){
 
 }
 
+void real_rotation (int n){
+	int i, j, k = n-1;
+	int temp;
+	int matrix[n][n];
+	int count = 1;
+
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++){
+			matrix[i][j] = count;
+			count++;
+		}
+	}
+
+	for(j = 0; j < n/2; j++){
+		int m = k;
+		for(i = j; i < k; i++){
+			temp = matrix[j][i];
+			matrix[j][i] = matrix[m][j];
+			matrix[m][j] = matrix[k][m];
+			matrix[k][m] = matrix[i][k];
+			matrix[i][k] = temp;
+			m--;
+		}
+		k--;
+	}
+
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++){
+			cout<<matrix[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+
+}
+
+
 int main(){
 	int n = 5;
+	int m = 6;
 	matrix_rotation(n);
-
+	cout<<"Real rotation"<<endl;
+	real_rotation(n);
+	cout<<"--------------------------------"<<endl;
+	/*matrix_rotation(m);
+	cout<<"Real rotation"<<endl;
+	real_rotation(m);
+*/
 	return 0;
 }
