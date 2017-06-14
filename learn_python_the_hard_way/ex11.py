29eeec14f834ee65461808db8a53ae2180bde630
aulon <aulonkuqi@gmail.com>
2017-06-14 23:19:10 +0200

    Added ex 11, 12, 13, 14

---
+++ learn_python_the_hard_way/ex11.py
@@ -0,0 +1,8 @@
+print "How old are you?", #the comma reads the input in the same line
+age = raw_input();
+print "How tall are you?",
+height = raw_input();
+print "How much do you weight?",
+weight = raw_input();
+
+print "So, you are %r old, %r tall, and %r heavy." % (age, height, weight)
